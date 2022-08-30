d9e2da9b27603ced1fc1ed8084c215d90eb62f3c
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-08-30 18:19:26 -0300

    Criação do jogo da memória inspirado no Genius

---
+++ index.js
@@ -0,0 +1,106 @@
+let order = [];
+let clickedOrder = [];
+let pontos = 0;
+
+//0 = verde
+//1 = vermelho
+//2 = amarelo
+//3 = azul
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
+//cria ordem aletoria de cores
+let embaralhar = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i in order) {
+        let elementColor = corDoElemento(order[i]);
+        acender(elementColor, Number(i) + 1);
+    }
+}
+
+//acende a proxima cor
+let acender = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    });
+}
+
+//verificar se o usuário clicou nos botões corretamente
+let conferirOrdem = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            perdeuAPartida();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length) {
+        alert(`Pontuação: ${pontos}\nVocê acertou! Iniciando próximo nível!`);
+        proximoNivel();
+    }
+}
+
+//funcao para o clique do usuario
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    corDoElemento(color).classList.add('selected');
+
+    setTimeout(() => {
+        corDoElemento(color).classList.remove('selected');
+        conferirOrdem();
+    },250);
+}
+
+//funcao que retorna a cor
+let corDoElemento = (color) => {
+    if(color == 0) {
+        return green;
+    } else if(color == 1) {
+        return red;
+    } else if (color == 2) {
+        return yellow;
+    } else if (color == 3) {
+        return blue;
+    }
+}
+
+//funcao para proximo nivel
+let proximoNivel = () => {
+    pontos++;
+    embaralhar();
+}
+
+//funcao para game over
+let perdeuAPartida = () => {
+    alert(`Pontuação: ${pontos}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+    playGame();
+}
+
+//funcao de inicio do jogo
+let playGame = () => {
+    alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
+    pontos = 0;
+
+    proximoNivel();
+}
+
+//eventos de clique para as cores
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+
+//inicio do jogo
+playGame();
